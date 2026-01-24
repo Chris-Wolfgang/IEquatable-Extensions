bbcf528947191216d271e2dc89cdf9c1dab597ad
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2026-01-23 22:36:49 -0500

    Restore and document IsInSet/IsNotInSet extension methods
    Re-enabled and documented all IsInSet and IsNotInSet overloads for IEquatable<T>, including support for arrays, IEnumerable, and ICollection. Added null checks and necessary using directives.

--- src/Wolfgang.Extensions.IEquatable/IEquatableExtensions.cs
+++ src/Wolfgang.Extensions.IEquatable/IEquatableExtensions.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Wolfgang.Extensions.IEquatable;
 
 /// <summary>
@@ -42,191 +46,191 @@ public static class IEquatableExtensions
     }
 
 
-    //    /// <summary>
-    //    /// Determines if the item is in the specified set of items.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="t1">The first item in the set.</param>
-    //    /// <param name="t2">The second item in the set.</param>
-    //    /// <param name="t3">The third item in the set.</param>
-    //    /// <typeparam name="T">The type of the items in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsInSet<T>(this T? item, T? t1, T? t2, T? t3) where T : IEquatable<T>
-    //#else
-    //    public static bool IsInSet<T>(this T item, T t1, T t2, T t3) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        return Equals(item, t1) || Equals(item, t2) || Equals(item, t3);
-    //    }
+    /// <summary>
+    /// Determines if the item is in the specified set of items.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="t1">The first item in the set.</param>
+    /// <param name="t2">The second item in the set.</param>
+    /// <param name="t3">The third item in the set.</param>
+    /// <typeparam name="T">The type of the items in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+#if NET5_0_OR_GREATER
+        public static bool IsInSet<T>(this T? item, T? t1, T? t2, T? t3) where T : IEquatable<T>
+#else
+    public static bool IsInSet<T>(this T item, T t1, T t2, T t3) where T : IEquatable<T>
+#endif
+    {
+        return Equals(item, t1) || Equals(item, t2) || Equals(item, t3);
+    }
 
 
 
-    //    /// <summary>
-    //    /// Determines if the item is in the specified set of set.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="set">The set of items to search.</param>
-    //    /// <typeparam name="T">The type of the set in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //    /// <exception cref="ArgumentNullException">set is null</exception>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsInSet<T>(this T? item, params T?[] set) where T : IEquatable<T>
-    //#else
-    //    public static bool IsInSet<T>(this T item, params T[] set) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        if (set == null)
-    //        {
-    //            throw new ArgumentNullException(nameof(set));
-    //        }
+    /// <summary>
+    /// Determines if the item is in the specified set of set.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="set">The set of items to search.</param>
+    /// <typeparam name="T">The type of the set in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">set is null</exception>
+#if NET5_0_OR_GREATER
+        public static bool IsInSet<T>(this T? item, params T?[] set) where T : IEquatable<T>
+#else
+    public static bool IsInSet<T>(this T item, params T[] set) where T : IEquatable<T>
+#endif
+    {
+        if (set == null)
+        {
+            throw new ArgumentNullException(nameof(set));
+        }
 
-    //        return set.Contains(item);
-    //    }
-
-
-
-    //    /// <summary>
-    //    /// Determines if the item is in the specified set of set.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="set">The set of items to search.</param>
-    //    /// <typeparam name="T">The type of the set in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //    /// <exception cref="ArgumentNullException">set is null</exception>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsInSet<T>(this T? item, IEnumerable<T?> set) where T : IEquatable<T>
-    //#else
-    //    public static bool IsInSet<T>(this T item, IEnumerable<T> set) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        if (set == null)
-    //        {
-    //            throw new ArgumentNullException(nameof(set));
-    //        }
-
-    //        return set.Contains(item);
-    //    }
+        return set.Contains(item);
+    }
 
 
 
-    //    /// <summary>
-    //    /// Determines if the item is in the specified set of set.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="set">The set of items to search.</param>
-    //    /// <typeparam name="T">The type of the set in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //    /// <exception cref="ArgumentNullException">set is null</exception>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsInSet<T>(this T? item, ICollection<T?> set) where T : IEquatable<T>
-    //#else
-    //    public static bool IsInSet<T>(this T item, ICollection<T> set) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        if (set == null)
-    //        {
-    //            throw new ArgumentNullException(nameof(set));
-    //        }
+    /// <summary>
+    /// Determines if the item is in the specified set of set.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="set">The set of items to search.</param>
+    /// <typeparam name="T">The type of the set in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">set is null</exception>
+#if NET5_0_OR_GREATER
+        public static bool IsInSet<T>(this T? item, IEnumerable<T?> set) where T : IEquatable<T>
+#else
+    public static bool IsInSet<T>(this T item, IEnumerable<T> set) where T : IEquatable<T>
+#endif
+    {
+        if (set == null)
+        {
+            throw new ArgumentNullException(nameof(set));
+        }
 
-    //        return set.Contains(item);
-    //    }
-
-
-
-    //    /// <summary>
-    //    /// Determines if the item is not in the specified set of items.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="t1">The first item in the set.</param>
-    //    /// <param name="t2">The second item in the set.</param>
-    //    /// <typeparam name="T">The type of the items in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsNotInSet<T>(this T? item, T? t1, T? t2) where T : IEquatable<T>
-    //#else
-    //    public static bool IsNotInSet<T>(this T item, T t1, T t2) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        return !IsInSet(item, t1, t2);
-    //    }
+        return set.Contains(item);
+    }
 
 
 
-    //    /// <summary>
-    //    /// Determines if the item is not in the specified set of items.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="t1">The first item in the set.</param>
-    //    /// <param name="t2">The second item in the set.</param>
-    //    /// <param name="t3">The third item in the set.</param>
-    //    /// <typeparam name="T">The type of the items in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsNotInSet<T>(this T? item, T? t1, T? t2, T? t3) where T : IEquatable<T>
-    //#else
-    //    public static bool IsNotInSet<T>(this T item, T t1, T t2, T t3) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        return !IsInSet(item, t1, t2, t3);
-    //    }
+    /// <summary>
+    /// Determines if the item is in the specified set of set.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="set">The set of items to search.</param>
+    /// <typeparam name="T">The type of the set in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">set is null</exception>
+#if NET5_0_OR_GREATER
+        public static bool IsInSet<T>(this T? item, ICollection<T?> set) where T : IEquatable<T>
+#else
+    public static bool IsInSet<T>(this T item, ICollection<T> set) where T : IEquatable<T>
+#endif
+    {
+        if (set == null)
+        {
+            throw new ArgumentNullException(nameof(set));
+        }
+
+        return set.Contains(item);
+    }
 
 
 
-    //    /// <summary>
-    //    /// Determines if the item is not in the specified set of items.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="set">The items in the set.</param>
-    //    /// <typeparam name="T">The type of the items in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //    /// <exception cref="ArgumentNullException">set is null</exception>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsNotInSet<T>(this T? item, params T?[] set) where T : IEquatable<T>
-    //#else
-    //    public static bool IsNotInSet<T>(this T item, params T[] set) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        return !IsInSet(item, set);
-    //    }
+    /// <summary>
+    /// Determines if the item is not in the specified set of items.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="t1">The first item in the set.</param>
+    /// <param name="t2">The second item in the set.</param>
+    /// <typeparam name="T">The type of the items in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+#if NET5_0_OR_GREATER
+        public static bool IsNotInSet<T>(this T? item, T? t1, T? t2) where T : IEquatable<T>
+#else
+    public static bool IsNotInSet<T>(this T item, T t1, T t2) where T : IEquatable<T>
+#endif
+    {
+        return !IsInSet(item, t1, t2);
+    }
 
 
 
-    //    /// <summary>
-    //    /// Determines if the item is not in the specified set of set.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="set">The set in the set.</param>
-    //    /// <typeparam name="T">The type of the set in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //    /// <exception cref="ArgumentNullException">set is null</exception>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsNotInSet<T>(this T? item, IEnumerable<T?> set) where T : IEquatable<T>
-    //#else
-    //    public static bool IsNotInSet<T>(this T item, IEnumerable<T> set) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        return !IsInSet(item, set);
-    //    }
+    /// <summary>
+    /// Determines if the item is not in the specified set of items.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="t1">The first item in the set.</param>
+    /// <param name="t2">The second item in the set.</param>
+    /// <param name="t3">The third item in the set.</param>
+    /// <typeparam name="T">The type of the items in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+#if NET5_0_OR_GREATER
+        public static bool IsNotInSet<T>(this T? item, T? t1, T? t2, T? t3) where T : IEquatable<T>
+#else
+    public static bool IsNotInSet<T>(this T item, T t1, T t2, T t3) where T : IEquatable<T>
+#endif
+    {
+        return !IsInSet(item, t1, t2, t3);
+    }
 
 
 
-    //    /// <summary>
-    //    /// Determines if the item is not in the specified set of set.
-    //    /// </summary>
-    //    /// <param name="item">The item to search for.</param>
-    //    /// <param name="set">The set in the set.</param>
-    //    /// <typeparam name="T">The type of the set in the set.</typeparam>
-    //    /// <returns>True if the item is in the set; otherwise, false.</returns>
-    //    /// <exception cref="ArgumentNullException">set is null</exception>
-    //#if NET5_0_OR_GREATER
-    //    public static bool IsNotInSet<T>(this T? item, ICollection<T?> set) where T : IEquatable<T>
-    //#else
-    //    public static bool IsNotInSet<T>(this T item, ICollection<T> set) where T : IEquatable<T>
-    //#endif
-    //    {
-    //        return !IsInSet(item, set);
-    //    }
+    /// <summary>
+    /// Determines if the item is not in the specified set of items.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="set">The items in the set.</param>
+    /// <typeparam name="T">The type of the items in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">set is null</exception>
+#if NET5_0_OR_GREATER
+        public static bool IsNotInSet<T>(this T? item, params T?[] set) where T : IEquatable<T>
+#else
+    public static bool IsNotInSet<T>(this T item, params T[] set) where T : IEquatable<T>
+#endif
+    {
+        return !IsInSet(item, set);
+    }
+
+
+
+    /// <summary>
+    /// Determines if the item is not in the specified set of set.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="set">The set in the set.</param>
+    /// <typeparam name="T">The type of the set in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">set is null</exception>
+#if NET5_0_OR_GREATER
+        public static bool IsNotInSet<T>(this T? item, IEnumerable<T?> set) where T : IEquatable<T>
+#else
+    public static bool IsNotInSet<T>(this T item, IEnumerable<T> set) where T : IEquatable<T>
+#endif
+    {
+        return !IsInSet(item, set);
+    }
+
+
+
+    /// <summary>
+    /// Determines if the item is not in the specified set of set.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <param name="set">The set in the set.</param>
+    /// <typeparam name="T">The type of the set in the set.</typeparam>
+    /// <returns>True if the item is in the set; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">set is null</exception>
+#if NET5_0_OR_GREATER
+        public static bool IsNotInSet<T>(this T? item, ICollection<T?> set) where T : IEquatable<T>
+#else
+    public static bool IsNotInSet<T>(this T item, ICollection<T> set) where T : IEquatable<T>
+#endif
+    {
+        return !IsInSet(item, set);
+    }
 
 
 
