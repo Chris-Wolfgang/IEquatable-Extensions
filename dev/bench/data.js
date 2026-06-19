75ff10a3ea1fc5017155dd99757067e51172949f
github-action-benchmark <github@users.noreply.github.com>
2026-06-19 18:06:04 +0000

    add BenchmarkDotNet (benchmarkdotnet) benchmark result for e44cfd11c4dbfa94fb6bc57e5dd65b7501fad871

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780175114794,
+  "lastUpdate": 1781892363814,
   "repoUrl": "https://github.com/Chris-Wolfgang/IEquatable-Extensions",
   "entries": {
     "BenchmarkDotNet": [
@@ -90,6 +90,96 @@ window.BENCHMARK_DATA = {
             "value": 20.980213691790897,
             "unit": "ns",
             "range": "± 0.28080639529485346"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com",
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang"
+          },
+          "distinct": true,
+          "id": "e44cfd11c4dbfa94fb6bc57e5dd65b7501fad871",
+          "message": "build(deps): bump actions/checkout in the github-actions group\n\nBumps the github-actions group with 1 update: [actions/checkout](https://github.com/actions/checkout).\n\n\nUpdates `actions/checkout` from 6 to 7\n- [Release notes](https://github.com/actions/checkout/releases)\n- [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/actions/checkout/compare/v6...v7)\n\n---\nupdated-dependencies:\n- dependency-name: actions/checkout\n  dependency-version: '7'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n  dependency-group: github-actions\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-06-19T14:03:18-04:00",
+          "tree_id": "74c5d07609e07ef619eb74e8e04b60cfa30d19f6",
+          "url": "https://github.com/Chris-Wolfgang/IEquatable-Extensions/commit/e44cfd11c4dbfa94fb6bc57e5dd65b7501fad871"
+        },
+        "date": 1781892361902,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_1",
+            "value": 26.72284287214279,
+            "unit": "ns",
+            "range": "± 0.09172642703441747"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_2",
+            "value": 52.839970211187996,
+            "unit": "ns",
+            "range": "± 0.3829752406520107"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_3",
+            "value": 78.42506923278172,
+            "unit": "ns",
+            "range": "± 0.21967293031814134"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_ParamsArray",
+            "value": 4.15081172188123,
+            "unit": "ns",
+            "range": "± 0.0037336551606528896"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_IEnumerable_FromList",
+            "value": 5.967766510943572,
+            "unit": "ns",
+            "range": "± 0.5665889301173478"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_IEnumerable_Iterator",
+            "value": 27.614313314358395,
+            "unit": "ns",
+            "range": "± 0.08863310464037796"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_ICollection_List",
+            "value": 5.672442103425662,
+            "unit": "ns",
+            "range": "± 0.012258308357575125"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsInSet_ICollection_HashSet",
+            "value": 2.693971591691176,
+            "unit": "ns",
+            "range": "± 0.005389957137656332"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsNotInSet_1",
+            "value": 26.464934051036835,
+            "unit": "ns",
+            "range": "± 0.10759471352207539"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.IsNotInSet_ParamsArray",
+            "value": 4.094335580865542,
+            "unit": "ns",
+            "range": "± 0.0035477107821221274"
+          },
+          {
+            "name": "Wolfgang.Extensions.IEquatable.Benchmarks.IEquatableExtensionsBenchmarks.NotEqual",
+            "value": 26.152511845032375,
+            "unit": "ns",
+            "range": "± 0.13583309022754778"
           }
         ]
       }
